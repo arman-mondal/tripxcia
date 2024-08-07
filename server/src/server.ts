12ce4983e75bf87a62b38a9f915a211fd790d48b
arman-mondal <arman@armanmondal.me>
2024-08-07 12:54:02 +0530

    Changes to be committed: 	new file:   server/log.json 	modified:   server/node_modules/.package-lock.json 	new file:   server/node_modules/@types/morgan/LICENSE 	new file:   server/node_modules/@types/morgan/README.md 	new file:   server/node_modules/@types/morgan/index.d.ts 	new file:   server/node_modules/@types/morgan/package.json 	new file:   server/node_modules/basic-auth/HISTORY.md 	new file:   server/node_modules/basic-auth/LICENSE 	new file:   server/node_modules/basic-auth/README.md 	new file:   server/node_modules/basic-auth/index.js 	new file:   server/node_modules/basic-auth/node_modules/safe-buffer/LICENSE 	new file:   server/node_modules/basic-auth/node_modules/safe-buffer/README.md 	new file:   server/node_modules/basic-auth/node_modules/safe-buffer/index.d.ts 	new file:   server/node_modules/basic-auth/node_modules/safe-buffer/index.js 	new file:   server/node_modules/basic-auth/node_modules/safe-buffer/package.json 	new file:   server/node_modules/basic-auth/package.json 	new file:   server/node_modules/morgan/HISTORY.md 	new file:   server/node_modules/morgan/LICENSE 	new file:   server/node_modules/morgan/README.md 	new file:   server/node_modules/morgan/index.js 	new file:   server/node_modules/morgan/node_modules/on-finished/HISTORY.md 	new file:   server/node_modules/morgan/node_modules/on-finished/LICENSE 	new file:   server/node_modules/morgan/node_modules/on-finished/README.md 	new file:   server/node_modules/morgan/node_modules/on-finished/index.js 	new file:   server/node_modules/morgan/node_modules/on-finished/package.json 	new file:   server/node_modules/morgan/package.json 	new file:   server/node_modules/on-headers/HISTORY.md 	new file:   server/node_modules/on-headers/LICENSE 	new file:   server/node_modules/on-headers/README.md 	new file:   server/node_modules/on-headers/index.js 	new file:   server/node_modules/on-headers/package.json 	modified:   server/package-lock.json 	modified:   server/package.json 	modified:   server/public/index.html 	modified:   server/src/server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,29 +3,35 @@ import dotenv from "dotenv";
 import { Router } from "./routes";
 import mongoose from "mongoose";
 import cors from "cors";
-
+import morgan from "morgan";
+import fs from "fs";
 dotenv.config();
 mongoose.connect(process.env.MONGO_URI as string);
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", function() {
-    console.log("Connected to MongoDB");
+  console.log("Connected to MongoDB");
 });
-
 
 const app: Express = express();
 const corsOptions: cors.CorsOptions = {
   origin: "*",
 };
 
+const accessLogStream = fs.createWriteStream("log.json", { flags: "a" });
+
+app.use(morgan("combined", { stream: accessLogStream }));
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const port = process.env.PORT || 3000;
 
-app.use('/',Router);
+app.use('/', Router);
+app.get('/system',(req:Request,res:Response)=>{
 
+  res.status(200).json(fs.readFileSync('log.json','utf-8'));
+})
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
 });
